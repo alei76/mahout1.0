ba2244611b83a43287168726655bb0825f57d77d
fansy1990 <fansy1990@foxmail.com>
2015-07-30 17:54:37 +0800

    mahout环境第一次配置

---
+++ src/main/java/com/fz/service/TestService.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.fz.service;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.fz.dao.BaseDao;
+import com.fz.model.User;
+import com.fz.util.HUtils;
+
+/**
+ * @author fansy
+ * @date 2015-2-9
+ */
+@Service("testService")
+public class TestService {
+	private Logger log = LoggerFactory.getLogger(TestService.class);
+	@Resource
+	private BaseDao<User> baseDao;
+	/**
+	 * 保存测试完成
+	 */
+	public int saveUser(){
+		User u = new User();
+		u.setGender("男");
+		u.setUserId("123456");
+		int num =(Integer)baseDao.save(u);
+//		System.out.println("条数："+num);
+		log.info("条数："+num);
+		return num;
+	}
+	/**
+	 * 更新测试完成
+	 */
+	public void updateUser(){
+		
+	}
+	/**
+	 * 获取HDFS文件信息
+	 * @param input
+	 * @return
+	 * @throws IOException 
+	 */
+	public String getHdfsFiles(String input) throws IOException{
+		
+		return HUtils.getHdfsFiles(input, true);
+	}
+	
+}
